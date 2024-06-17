cc6475d1c461e27b18ed932f842c87b06e850505
aymen <aymen2255@hotmail.fr>
2024-06-18 00:28:34 +0200

    test list component

--- front/src/app/features/sessions/components/list/list.component.spec.ts
+++ front/src/app/features/sessions/components/list/list.component.spec.ts
@@ -6,10 +6,15 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { ListComponent } from './list.component';
+import { SessionApiService } from '../../services/session-api.service';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
+  let sessionApiService: SessionApiService;
+  let sessionService: SessionService;
 
   const mockSessionService = {
     sessionInformation: {
@@ -25,12 +30,41 @@ describe('ListComponent', () => {
     })
       .compileComponents();
 
+    
     fixture = TestBed.createComponent(ListComponent);
     component = fixture.componentInstance;
+    sessionApiService = TestBed.inject(SessionApiService);
+    sessionService = TestBed.inject(SessionService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should call all method of SessionApiService', () => {
+    const sessionApiServiceSpy = jest.spyOn(sessionApiService, 'all').mockReturnValue(of([]));
+    fixture = TestBed.createComponent(ListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    expect(component.sessions$).toBeDefined();
+    expect(sessionApiServiceSpy).toHaveBeenCalled();
+  });
+  
+  it('should not display create button when user is not admin', () => {
+    mockSessionService.sessionInformation.admin = false;
+    fixture.detectChanges();
+    const createButton = fixture.debugElement.query(By.css('button[routerLink="create"]'));
+    expect(createButton).toBeFalsy();
+  });
+  
+  it('should display create button when user is admin', () => {
+    mockSessionService.sessionInformation.admin = true;
+    fixture.detectChanges();
+    const createButton = fixture.debugElement.query(By.css('button[routerLink="create"]'));
+    expect(createButton).toBeTruthy();
+  });
+  
+
 });
