bea4cc4d122c8562cdd86c990cb794fd4eccd614
aymen <aymen2255@hotmail.fr>
2024-06-21 00:55:26 +0200

    end to end test login component

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,6 +1,10 @@
 describe('Login spec', () => {
+  beforeEach(() => {
+    // Visiter la page de connexion avant chaque test
+    cy.visit('/login');
+  });
+
   it('Login successfull', () => {
-    cy.visit('/login')
 
     cy.intercept('POST', '/api/auth/login', {
       body: {
@@ -24,4 +28,16 @@ describe('Login spec', () => {
 
     cy.url().should('include', '/sessions')
   })
+
+  it('should display an error message when login fails', () => {
+    // Remplir le formulaire avec des informations d'identification incorrectes
+    cy.get('#mat-input-0').type('wrong.email@example.com');
+    cy.get('#mat-input-1').type('wrong-password');
+
+    // Soumettre le formulaire
+    cy.get('.mat-raised-button').click();
+
+    // Vérifier si le message d'erreur est affiché
+    cy.get('.error').should('contain', 'An error occurred');
+  });
 });
